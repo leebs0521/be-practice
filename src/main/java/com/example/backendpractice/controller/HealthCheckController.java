8ad19b0f4bc679e79e46edb954f7a19dacd51f06
leebs0521 <leebs0521@gmail.com>
2024-07-10 13:52:43 +0900

    Feat HealthCheckController

---
+++ src/main/java/com/example/backendpractice/controller/HealthCheckController.java
@@ -0,0 +1,33 @@
+package com.example.backendpractice.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HealthCheckController {
+
+    @Value("${server.env}")
+    private String env;
+    @Value("${server.port}")
+    private String serverPort;
+    @Value("${serverName}")
+    private String serverName;
+    @GetMapping("/hc")
+    public ResponseEntity<?> healthCheck() {
+        Map<String, String> responseData = new HashMap<>();
+        responseData.put("serverName", serverName);
+        responseData.put("env", env);
+        responseData.put("serverPort", serverPort);
+        return ResponseEntity.ok(responseData);
+    }
+
+    @GetMapping("/env")
+    public ResponseEntity<?> getEnv() {
+        return ResponseEntity.ok(env);
+    }
+}
